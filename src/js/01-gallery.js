6f1ac61d09b15c816df6714ed02011d82b4b2d1b
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-02-09 21:42:40 +0200

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerEl = document.querySelector(".gallery");
+
+const markup = galleryItems.map(({preview, original, description}) =>
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`).join("");
+
+galleryContainerEl.innerHTML = markup;
+
+
+var lightbox = new SimpleLightbox('.gallery a', {
+
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: '250ms'
+
+});
+

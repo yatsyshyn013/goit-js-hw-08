3eee7404ffc4d6da3321c3d7421d14f46274041b
Maksym Yatsyshyn <max.yatsushyn@gmail.com>
2023-02-10 00:08:03 +0200

    task_2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY = "videoplayer-current-time";
+
+const setTime = throttle(function(evt) {
+        localStorage.setItem(KEY, evt.seconds);
+}, 1000);
+
+player.on('timeupdate', setTime);
+
+player.setCurrentTime(localStorage.getItem(KEY));
 
